1360e8107ea736e30fe5831d1e5d8482383896cd
glen <glenwojnar@yahoo.com>
2022-12-06 19:29:58 -0500

    phase 3 completed

--- command.js
+++ command.js
@@ -1,1 +1,2 @@
 // npx sequelize-cli model:generate --name Tree --attributes id:integer,tree:string,location:string,heightFt:float,groundCircumferenceFt:float
+// npx sequelize-cli model:generate --name Insect --attributes id:integer,name:string,description:string,territory:string,fact:string,millimeters:float
